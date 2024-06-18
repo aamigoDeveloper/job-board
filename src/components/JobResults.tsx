fc63aad50d2059fc7c13a620a15fcb7c426a13ad
aamigo <aamigodeveloper@gmail.com>
2024-06-18 23:21:45 +0330

    no job found with the given search filters added

--- src/components/JobResults.tsx
+++ src/components/JobResults.tsx
@@ -2,16 +2,21 @@ import prisma from "@/lib/db"
 import Link from "next/link"
 import JobItem from "./JobItem"
 
-export default async function JobResults() {  
+export default async function JobResults() {
   const jobs = await prisma.job.findMany()
 
   return (
     <div className="grow space-y-3">
-    {jobs.map((job) => (
-      <Link href={`/jobs/${job.slug}`} key={job.id} className="block">
-        <JobItem job={job} />
-      </Link>
-    ))}
-  </div>
+      {jobs.map((job) => (
+        <Link href={`/jobs/${job.slug}`} key={job.id} className="block">
+          <JobItem job={job} />
+        </Link>
+      ))}
+      {jobs.length === 0 && (
+        <p className="m-auto text-center">
+          No Jobs found. Try adjusting your search filters.
+        </p>
+      )}
+    </div>
   )
 }
