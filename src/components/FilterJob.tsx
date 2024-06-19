8ac290f6be9a0991cd2ac642498c58a5fabce9fa
aamigo <aamigodeveloper@gmail.com>
2024-06-19 09:10:56 +0330

    filter action

--- src/components/FilterJob.tsx
+++ src/components/FilterJob.tsx
@@ -1,4 +1,4 @@
-import { JobFilterValues } from "@/lib/validation"
+import { JobFilterValues, jobFilterSchema } from "@/lib/validation"
 import FormSubmittingButton from "./FormSubmittingButton"
 import Select from "./Select"
 import { Checkbox } from "./ui/checkbox"
@@ -6,6 +6,24 @@ import { Input } from "./ui/input"
 import { Label } from "./ui/label"
 import { jobTypes } from "@/lib/job-types"
 import prisma from "@/lib/db"
+import { redirect } from "next/navigation"
+
+async function filterJobs(formData: FormData) {
+  "use server"
+
+  const values = Object.fromEntries(formData.entries())
+
+  const { q, type, location, remote } = jobFilterSchema.parse(values)
+
+  const searchParams = new URLSearchParams({
+    ...(q && { q: q.trim() }),
+    ...(type && { type }),
+    ...(location && { location }),
+    ...(remote && { remote: "true" }),
+  })
+
+  redirect(`/?${searchParams.toString()}`)
+}
 
 interface FilterJobProps {
   defaultValues: JobFilterValues
@@ -26,7 +44,7 @@ export default async function FilterJob({
 
   return (
     <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4 md:w-[260px]">
-      <form>
+      <form action={filterJobs}>
         <div className="space-y-4">
           <div className="flex flex-col gap-2">
             <Label htmlFor="q">Search</Label>
