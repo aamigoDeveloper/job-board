994ba25400bb5a5ce3f8bf91f4aede134e336575
aamigo <aamigodeveloper@gmail.com>
2024-06-19 08:52:19 +0330

    Setting up default values for filter form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 import FilterJob from "@/components/FilterJob";
 import JobResults from "@/components/JobResults";
+import { JobFilterValues } from "@/lib/validation";
+
+interface PageProps {
+  searchParams: {
+    q?: string
+    type?: string
+    location?: string
+    remote?: string
+  }
+}
 
 
-export default async function Home() {
+export default async function Home({ searchParams: { q, type, location, remote } }: PageProps) {
+  const filterValues: JobFilterValues = {
+    q,
+    type,
+    location,
+    remote: remote === "true"
+  }
 
   return (
     <main className="max-w-5xl m-auto my-10 px-3 space-y-10">
@@ -11,7 +27,7 @@ export default async function Home() {
         <p className="text-muted-foreground">Find your dream job.</p>
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
-        <FilterJob />
+        <FilterJob defaultValues={filterValues} />
         <JobResults />
       </section>
     </main>
--- src/components/FilterJob.tsx
+++ src/components/FilterJob.tsx
@@ -1,34 +1,46 @@
+import { JobFilterValues } from "@/lib/validation"
 import FormSubmittingButton from "./FormSubmittingButton"
 import Select from "./Select"
 import { Checkbox } from "./ui/checkbox"
 import { Input } from "./ui/input"
 import { Label } from "./ui/label"
 
-export default function FilterJob() {
+interface FilterJobProps {
+  defaultValues: JobFilterValues
+}
+
+export default function FilterJob({
+  defaultValues: { q, type, location, remote },
+}: FilterJobProps) {
   return (
     <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4">
       <form>
         <div className="space-y-4">
           <div className="flex flex-col gap-2">
             <Label htmlFor="q">Search</Label>
-            <Input placeholder="Type, company, etc..." id="q" name="q" />
+            <Input
+              placeholder="Type, company, etc..."
+              id="q"
+              name="q"
+              defaultValue={q}
+            />
           </div>
           <div className="flex flex-col gap-2">
             <Label htmlFor="types">Types</Label>
-            <Select id="types" name="types">
+            <Select id="types" name="types" defaultValue={type || ""}>
               <option value="">All Types</option>
               {}
             </Select>
           </div>
           <div className="flex flex-col gap-2">
             <Label htmlFor="location">Location</Label>
-            <Select id="location" name="location">
+            <Select id="location" name="location" defaultValue={location || ""}>
               <option value="">All Locations</option>
               {}
             </Select>
           </div>
           <div className="flex items-center gap-2">
-            <Checkbox id="remote" name="remote" />
+            <Checkbox id="remote" name="remote" defaultChecked={remote} />
             <Label htmlFor="remote">Remote</Label>
           </div>
           <FormSubmittingButton className="w-full">
