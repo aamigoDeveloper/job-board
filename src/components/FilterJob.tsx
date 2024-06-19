a75f158bc7b2761d613fc8f42f28c308e74d2f27
aamigo <aamigodeveloper@gmail.com>
2024-06-19 09:01:47 +0330

    displaying types and distinct locations in select components

--- src/components/FilterJob.tsx
+++ src/components/FilterJob.tsx
@@ -4,16 +4,28 @@ import Select from "./Select"
 import { Checkbox } from "./ui/checkbox"
 import { Input } from "./ui/input"
 import { Label } from "./ui/label"
+import { jobTypes } from "@/lib/job-types"
+import prisma from "@/lib/db"
 
 interface FilterJobProps {
   defaultValues: JobFilterValues
 }
 
-export default function FilterJob({
+export default async function FilterJob({
   defaultValues: { q, type, location, remote },
 }: FilterJobProps) {
+  const distincsLocations = (await prisma.job
+    .findMany({
+      where: { approved: true },
+      select: { location: true },
+      distinct: ["location"],
+    })
+    .then((locations) =>
+      locations.map(({ location }) => location).filter(Boolean)
+    )) as string[]
+
   return (
-    <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4">
+    <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4 md:w-[260px]">
       <form>
         <div className="space-y-4">
           <div className="flex flex-col gap-2">
@@ -29,14 +41,22 @@ export default function FilterJob({
             <Label htmlFor="types">Types</Label>
             <Select id="types" name="types" defaultValue={type || ""}>
               <option value="">All Types</option>
-              {}
+              {jobTypes.map((type) => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
             </Select>
           </div>
           <div className="flex flex-col gap-2">
             <Label htmlFor="location">Location</Label>
             <Select id="location" name="location" defaultValue={location || ""}>
               <option value="">All Locations</option>
-              {}
+              {distincsLocations.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
             </Select>
           </div>
           <div className="flex items-center gap-2">
