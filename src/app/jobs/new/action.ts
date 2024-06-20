92eeeab52aafac69a700289273ea6507bcacd57a
aamigo <aamigodeveloper@gmail.com>
2024-06-20 22:00:13 +0330

    Adding image

--- src/app/jobs/new/action.ts
+++ src/app/jobs/new/action.ts
@@ -4,7 +4,9 @@ import prisma from "@/lib/db"
 import { toSlug } from "@/lib/utils"
 import { createJobSchema } from "@/lib/validation"
 import { nanoid } from "nanoid"
+import { put } from "@vercel/blob"
 import { redirect } from "next/navigation"
+import path from "path"
 
 export const createJob = async (formData: FormData) => {
   const values = Object.fromEntries(formData.entries())
@@ -15,6 +17,7 @@ export const createJob = async (formData: FormData) => {
     companyName,
     locationType,
     location,
+    companyLogo,
     applicationEmail,
     applicationUrl,
     description,
@@ -22,6 +25,21 @@ export const createJob = async (formData: FormData) => {
   } = createJobSchema.parse(values)
 
   const slug = `${toSlug(title)}-${nanoid(10)}`
+
+  let companyLogoUrl: string | undefined = undefined
+
+  if (companyLogo) {
+    const blob = await put(
+      `company_logos/${slug}${path.extname(companyLogo.name)}`,
+      companyLogo,
+      {
+        access: "public",
+        addRandomSuffix: false,
+      }
+    )
+
+    companyLogoUrl = blob.url
+  }
 
   await prisma.job.create({
     data: {
